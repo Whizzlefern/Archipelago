dbd5ba943c1561fa200dfcad61a4357d6deea2f4
whizzlefern <turmericrain@gmail.com>
2026-08-31 21:27:10 -0700

    many many fixes

--- worlds/dqm2/rom.py
+++ worlds/dqm2/rom.py
@@ -502,6 +502,7 @@ def edit_overworld_boss(patch, encounter, mon_id) -> None:
     elif encounter == 0x44:
         # Estria Castle
         write_bytes(patch, get_full_addr(0x69, 0x5d1b), mon_id)
+        write_bytes(patch, get_full_addr(0x69, 0x5d3d), mon_id)
     elif encounter == 0x199:
         # Norden Castle
         write_bytes(patch, get_full_addr(0x69, 0x5331), mon_id)
